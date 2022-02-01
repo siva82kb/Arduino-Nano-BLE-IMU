5254c3a9f51d3ab262e7f4569105b30872522379
siva82kb <siva82kb@gmail.com>
2022-02-01 13:51:57 +0530

    Added time to the BLE packet.

--- ble_imu/ble_imu.ino
+++ ble_imu/ble_imu.ino
@@ -5,21 +5,27 @@
 #include <ArduinoBLE.h>
 #include <Arduino_LSM6DS3.h>
 
-#define DEBUG true
+#define DEBUG false
 
 // These UUIDs have been randomly generated. - they must match between the Central and Peripheral devices
 // Any changes you make here must be suitably made in the Python program as well
 
 BLEService nanoIMUService("13012F00-F8C3-4F4A-A8F4-15CD926DA146"); // BLE Service
 // Accelerometer and Gyroscope characteristics
-BLECharacteristic acclGyroCharacteristic("13012F01-F8C3-4F4A-A8F4-15CD926DA146", BLENotify, 24);
+BLECharacteristic acclGyroCharacteristic("13012F01-F8C3-4F4A-A8F4-15CD926DA146", BLENotify, 28);
 
 // IMU data packet
 typedef union {
-  float accgyr[6];
-  uint8_t bytes[24];
+  float timeaccgyr[7];
+  uint8_t bytes[28];
 } imuunion_t;
 
+typedef union {
+    unsigned long data;
+    uint8_t bytes[4];
+} ulongunion_t;
+
+ulongunion_t _time;
 imuunion_t _imudata;
 
 void setup() {
@@ -51,7 +57,7 @@ void setup() {
   BLE.addService(nanoIMUService);
 
   // set the initial value for the characeristic:
-  acclGyroCharacteristic.writeValue(_imudata.bytes, 24);
+  acclGyroCharacteristic.writeValue(_imudata.bytes, 28);
 
   // start advertising
   BLE.advertise();
@@ -72,9 +78,15 @@ void loop() {
     // while the central is still connected to peripheral:
     while (central.connected()) {
         if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()) {
-            IMU.readAcceleration(_imudata.accgyr[0], _imudata.accgyr[1], _imudata.accgyr[2]);
-            IMU.readGyroscope(_imudata.accgyr[3], _imudata.accgyr[4], _imudata.accgyr[5]);
-            acclGyroCharacteristic.writeValue(_imudata.bytes, 24);
+            // Get current time 
+            _time.data = micros();
+            // Update first four bytes of IMU data union.
+            for (int i = 0; i < 4;i ++) {
+                _imudata.bytes[i] = _time.bytes[i];
+            }
+            IMU.readAcceleration(_imudata.timeaccgyr[1], _imudata.timeaccgyr[2], _imudata.timeaccgyr[3]);
+            IMU.readGyroscope(_imudata.timeaccgyr[4], _imudata.timeaccgyr[5], _imudata.timeaccgyr[6]);
+            acclGyroCharacteristic.writeValue(_imudata.bytes, 28);
         }
     }
     
--- nanobleimu.py
+++ nanobleimu.py
@@ -8,6 +8,7 @@ from multiprocessing import ProcessError
 import struct
 import sys
 from typing import Dict
+from importlib_metadata import csv
 import keyboard
 
 import bleak
@@ -22,7 +23,7 @@ IMU_UUID = '13012F01-F8C3-4F4A-A8F4-15CD926DA146'
 # Class for handling BLE communication with a Nano board for receiving IMU data.
 class NanoIMUBLEClient(object):
     
-    def __init__(self, uuid:str) -> None:
+    def __init__(self, uuid:str, csvout:bool=False) -> None:
         super().__init__()
         self._client = None
         self._device = None
@@ -30,8 +31,10 @@ class NanoIMUBLEClient(object):
         self._running = False
         self._uuid = uuid
         self._found = False
-        self._data = {"ax": 0.0, "ay": 0.0, "az": 0.0,
-                      "gx": 0.0, "gy": 0.0, "gz": 0.0} 
+        self._data = {"time": 0, 
+                      "ax": 0.0, "ay": 0.0, "az": 0.0,
+                      "gx": 0.0, "gy": 0.0, "gz": 0.0}
+        self._csvout = csvout 
         self.newdata = False
         self.printdata = True
     
@@ -80,8 +83,9 @@ class NanoIMUBLEClient(object):
             while self._connected:
                 if self._running:
                     # Print data.
-                    if self.printdata:
+                    if self.printdata and self.newdata:
                         self.print_newdata()
+                        self.newdata = False
                     await asyncio.sleep(0)
     
     async def disconnect(self) -> None:
@@ -104,30 +108,41 @@ class NanoIMUBLEClient(object):
             await self._client.stop_notify(self._uuid)
         
     def newdata_hndlr(self, sender, data):
-        self._data['ax'] = struct.unpack('<f', bytes(data[0:4]))[-1]
-        self._data['ay'] = struct.unpack('<f', bytes(data[4:8]))[-1]
-        self._data['az'] = struct.unpack('<f', bytes(data[8:12]))[-1]
-        self._data['gx'] = struct.unpack('<f', bytes(data[12:16]))[-1]
-        self._data['gy'] = struct.unpack('<f', bytes(data[16:20]))[-1]
-        self._data['gz'] = struct.unpack('<f', bytes(data[20:24]))[-1]
+        self._data['time'] = struct.unpack('<L', bytes(data[0:4]))[-1]
+        self._data['ax'] = struct.unpack('<f', bytes(data[4:8]))[-1]
+        self._data['ay'] = struct.unpack('<f', bytes(data[8:12]))[-1]
+        self._data['az'] = struct.unpack('<f', bytes(data[12:16]))[-1]
+        self._data['gx'] = struct.unpack('<f', bytes(data[16:20]))[-1]
+        self._data['gy'] = struct.unpack('<f', bytes(data[20:24]))[-1]
+        self._data['gz'] = struct.unpack('<f', bytes(data[24:28]))[-1]
         self.newdata = True
     
     def print_newdata(self) -> None:
-        _str = ("\r Accl: " +
-                f"{self.data['ax']:+1.3f}, " + 
-                f"{self.data['ay']:+1.3f}, " + 
-                f"{self.data['az']:+1.3f} | " +
-                "Gyro: " +
-                f"{self.data['gx']:+3.3f}, " +
-                f"{self.data['gy']:+3.3f}, " +
-                f"{self.data['gz']:+3.3f}")
+        if self._csvout:
+            _str = (f"{self.data['time']/1000000.0:+3.3f}, " +
+                    f"{self.data['ax']:+1.3f}, " + 
+                    f"{self.data['ay']:+1.3f}, " + 
+                    f"{self.data['az']:+1.3f}, " +
+                    f"{self.data['gx']:+3.3f}, " +
+                    f"{self.data['gy']:+3.3f}, " +
+                    f"{self.data['gz']:+3.3f}\n")
+        else:
+            _str = (f"\r Time: {self.data['time']/1000000.0:+3.3f} | " +
+                    "Accl: " +
+                    f"{self.data['ax']:+1.3f}, " + 
+                    f"{self.data['ay']:+1.3f}, " + 
+                    f"{self.data['az']:+1.3f} | " +
+                    "Gyro: " +
+                    f"{self.data['gx']:+3.3f}, " +
+                    f"{self.data['gy']:+3.3f}, " +
+                    f"{self.data['gz']:+3.3f}")
         sys.stdout.write(_str)
         sys.stdout.flush()
 
 
 async def run():
     # Create a new IMU client.
-    imu_client = NanoIMUBLEClient(IMU_UUID)
+    imu_client = NanoIMUBLEClient(IMU_UUID, False)
     await imu_client.connect()
 
 
